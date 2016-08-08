347a100ce434d5f95623288fc2e7619b54ce63df
quincho77 <ppasosv@hotmail.com>
2016-08-08 11:55:37 -0600

    Se agregó método que coloca bordes para formar el recibo.

--- RecibosDePrestamos/RecibosDePrestamos/ExportarAExcel.cs
+++ RecibosDePrestamos/RecibosDePrestamos/ExportarAExcel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel ;
 using System.Windows.Forms;
 
 namespace Recibos
@@ -19,13 +19,14 @@ namespace Recibos
             Worksheet hoja = libro.Worksheets.Add();   // nueva hoja de excel
 
             hoja.Name = "SEMANAL";
-            ajustarFilas(hoja);
+            ajustarTamanioFilas(hoja);
             ajustarAnchoEnFilas(hoja);
+            ajustarBordes(hoja);
             appExcel.Visible = true;
         }// fin del método start_Up
 
         // El siguiente método prepara el tamaño de las filas en la hoja de excel
-        public void ajustarFilas(Worksheet hoja)
+        public void ajustarTamanioFilas(Worksheet hoja)
         {
             int cursor = 1;    // Nos indica en fila esta posicionado
 
@@ -33,53 +34,55 @@ namespace Recibos
             fila.RowHeight = 9;     // tamaño de fila de 9pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
 
-            cursor = ajustarFilasRecibo(cursor, hoja);
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
 
             fila = hoja.Rows[cursor];
             fila.RowHeight = 9.75;  // tamaño de fila de 9.75pts
             cursor++;
+
+            cursor = ajustarTamanioFilasRecibo(cursor, hoja);
         }// fin del método ajustarFilas
 
         // El siguiente método ajusta el ancho de las filas en la columna A
@@ -104,13 +107,23 @@ namespace Recibos
             columna.ColumnWidth = 0.75;  // columna F ancho de 0.92pts
         }// fin del método ajustarAnchoEnFilas
 
+        // El siguiente método coloca bordes alrededor de las celdas que conforman
+        // el recibo
+        public void ajustarBordes(Worksheet hoja)
+        {
+
+            hoja.get_Range("A1", "B55").Borders[XlBordersIndex.xlInsideHorizontal].LineStyle = XlLineStyle.xlContinuous;
+            hoja.get_Range("A2", "C54").Borders[XlBordersIndex.xlInsideVertical].LineStyle = XlLineStyle.xlContinuous;
+            hoja.get_Range("D1", "E55").Borders[XlBordersIndex.xlInsideHorizontal].LineStyle = XlLineStyle.xlContinuous;
+            hoja.get_Range("C2", "F54").Borders[XlBordersIndex.xlInsideVertical].LineStyle = XlLineStyle.xlContinuous;
+        }// fin del método ajustarBordes
 
         // DE ESTE BLOQUE DE CÓDIGO EN ADELANTE SE ESCRIBEN LOS MÉTODOS QUE SON
         // INVOCADOS POR EL MÉTODO ajustarFilas
 
         // El siguiente método ajusta el tamaño de las filas que conforman
         // parte del recibo
-        public int ajustarFilasRecibo(int indice, Worksheet hoja)
+        public int ajustarTamanioFilasRecibo(int indice, Worksheet hoja)
         {
             for (int i = indice; i < indice + 5; i++)
             {
@@ -120,7 +133,7 @@ namespace Recibos
 
             indice = indice + 5;     // posicionamos el cursor
             return indice;
-        }// fin del método ajustarFilasRecibo
+        }// fin del método ajustarTamanioFilasRecibo
 
     }// fin de la clase ExportarAExcel
 
